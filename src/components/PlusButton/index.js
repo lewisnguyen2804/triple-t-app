d92e56732eabd2688385982e4ebd09f18a94394f
INI\triet.le <triet.le@terralogic.com>
2020-07-13 11:12:16 +0700

    Add PlusButton components

---
+++ src/components/PlusButton/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from "./PlusButton.module.css"
+
+export default function PlusButton() {
+    return (
+        <div className={classes.buttonWrapper}>
+            <div className={classes.PlusButton}></div>
+        </div>
+    )
+}
