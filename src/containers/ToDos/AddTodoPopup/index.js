c35a43ecf01cbfa8fc60d2fe3f700a51cfd6abba
INI\triet.le <triet.le@terralogic.com>
2020-07-13 15:42:44 +0700

    Add Popup and AddTodoPopup component

--- src/containers/ToDos/AddTodoPopup/index.js
+++ src/containers/ToDos/AddTodoPopup/index.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
+import classes from "./AddTodoPopup.module.css"
+import Popup from "../../../components/Popup"
 
 export default class AddTodoPopup extends Component {
-    render() {
+    state={
+        userInput:""
+    }
+
+    changeUserInputHandler(event){
+        this.setState({
+            userInput: event.target.value
+        })
+    }
+
+    submitInputHandler(){
+        this.props.addTodo(this.state.userInput)
+        this.setState({
+            userInput:""
+        })
+    }
+    render(props) {
         return (
-            <div>
-                <input type="text"/>
-                <button>Add</button>
-            </div>
+            <Popup popupStatus={this.props.popupStatus}>
+                <div className={classes.AddTodoPopup}>
+                    <input className={classes.AddTodoField} value={this.state.userInput} type="text" onChange={(e)=>this.changeUserInputHandler(e)}/>
+                    <button className={classes.Button} onClick={()=> this.submitInputHandler()}>Add</button>
+                </div>
+            </Popup>
         )
     }
 }
--- src/containers/ToDos/index.js
+++ src/containers/ToDos/index.js
@@ -2,41 +2,64 @@ import React, { Component } from 'react'
 import classes from "./Todos.module.css"
 import PlusButton from "../../components/PlusButton"
 import TodoList from "./TodoList"
+import AddTodoPopup from "./AddTodoPopup"
 
 export default class ToDos extends Component {
     state = {
         todos: [
             {id: 0,title: "Test dodo", isDone: false}
-        ]
+        ],
+        popup: false
     }
 
     toggleTodoStatus(selectedID) {
         // Find selected todo
         const selectedTodoIndex = this.state.todos.findIndex(todo => todo.id = selectedID)
-        this.setState(this.state.todos[selectedTodoIndex].isDone = !this.state.todos[selectedTodoIndex].isDone)        
+        const newTodos =[...this.state.todos] 
+        newTodos[selectedTodoIndex].isDone = !newTodos[selectedTodoIndex].isDone
+
+        this.setState({
+            todos: newTodos
+        })        
     }
 
     deleteTodo(selectedID) {
-        this.setState(this.state.todos.filter(todo => todo.id !== selectedID))
+        const newTodos = [...this.state.todos]
+        newTodos.filter(todo => todo.id !== selectedID)
+        this.setState({
+            todos: newTodos
+        })
     }
 
     addTodo(title) {
         const newTodo = {
             // newID = lasted todo's ID + 1
-            id: this.state.todos[this.state.todos.length].id + 1,
+            id: this.state.todos[this.state.todos.length - 1].id + 1,
             title : title,
             isDone: false
         }
-        this.setState(this.state.todos.push(newTodo))
+        const newTodos = [...this.state.todos]
+        newTodos.push(newTodo)
+        this.setState({
+            todos: newTodos
+        })
+        this.popupHandler()
     }
+
+    popupHandler() {
+        this.setState({
+            popup: !this.state.popup
+        })
+    }
+
 
     render() {
         return (
             <div className={classes.Todos}>
                <h1>All Todo</h1>
                <TodoList todos={this.state.todos} toggleTodoStatus={this.toggleTodoStatus} deleteTodo={this.deleteTodo}/>
-               {/* Add Todo popup*/}
-               <PlusButton/>
+               <AddTodoPopup popupStatus={this.state.popup} addTodo={(title)=>this.addTodo(title)}/>
+               <PlusButton clickHandler={()=> this.popupHandler()}/>
             </div>
         )
     }
