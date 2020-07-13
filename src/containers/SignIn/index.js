55cac06115d9f52dfaa6a4a52abe4b8320645109
trung1310 <trung.nguyen@terralogic.com>
2020-07-13 10:27:16 +0700

    create and design layout SignIn - PART-1

--- src/containers/SignIn/index.js
+++ src/containers/SignIn/index.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import classes from "./signIn.module.css"
+import React from "react";
+import classes from "./signIn.module.css";
+
 
 export default function SignIn() {
-    return (
-        <div>
-            <button className={classes.Button}>Click me</button>
-        </div>
-    )
+  return (
+    <div className={classes.signIn_Form}>
+
+      <h1 className={classes.signIn}>Sign in</h1>
+
+      <form className={classes.info_Form}>
+        <label>Your email:</label>
+        <input className={classes.email_ID} type="email" placeholder="Email..."/>
+
+        <label>Your pasword:</label>
+        <input className={classes.email_password} type="password" placeholder="Password"/>
+
+        <a className={classes.forgot_password} href="#forget">Forgot Password?</a>
+        <button className={classes.btn_signIn}>SIGN IN</button>
+        <label>or Sign in with:</label>
+
+      </form>
+    </div>
+  );
 }
