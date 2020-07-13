a3ff42e0c0b2584ab3562a0d07cbd154540c74cd
INI\lewis.nguyen <lewis.nguyen@terralogic.com>
2020-07-13 11:14:38 +0700

    done Sign Up page

---
+++ src/containers/SignUp/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import classes from "./signUp.module.css";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div className={classes.container}>
+                <div className={classes.signUpContainer}>
+                    <div className={classes.signUpArea}>
+                    <h1 className={classes.titleText}>
+                        Sign Up
+                    </h1>
+                    <form>
+                        <input type="email" placeholder="Your email" className={classes.inputBox} />
+                        <input type="password" placeholder="Your password" className={classes.inputBox}/>
+                        <div className={classes.agreeTerms}>
+                            <input type="checkbox" /> Agree to the <a href="#">Terms and Conditions</a>
+                        </div>
+                        
+                        <div className={classes.buttonArea}>
+                            <button className={classes.signUpButton}>Sign Up</button>
+                            <div>
+                                Have an account?
+                                <a href="/signin">Login</a>
+                            </div>
+                        </div>
+                    </form>
+                    </div>
+                    <div className={classes.socialLoginArea}>
+                        {/* <div className={classes.socialIcons}> */}
+                            <a href="#">
+                                <img className={classes.socialIcon} src={require('../../assets/facebook.png')} />
+                            </a>
+                            <a href="#">
+                                <img className={classes.socialIcon} src={require('../../assets/twitter.png')} />
+                            </a>
+                            <a href="#">
+                                <img className={classes.socialIcon} src={require('../../assets/youtube.png')} />
+                            </a>
+                        {/* </div> */}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
