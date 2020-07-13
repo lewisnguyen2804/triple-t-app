f134936b53314cc8e6f0c3ad5815ff0186d2512b
INI\lewis.nguyen <lewis.nguyen@terralogic.com>
2020-07-13 11:17:27 +0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import './App.module.css';
-import SignIn from "./containers/SignIn"
 import classes from './App.module.css';
+import SignIn from "./containers/SignIn";
+import SignUp from "./containers/SignUp";
+import ToDo from "./containers/ToDo";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className={classes.App}>
-      <SignIn/>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/toto">
+            <ToDo />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
