b08e7864ac366b0b21ea79084bbd7eb8d6f68351
INI\lewis.nguyen <lewis.nguyen@terralogic.com>
2020-07-13 09:37:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignIn from "./containers/SignIn"
+
 
 function App() {
   return (
     <div className="App">
-     
+      <SignIn/>
     </div>
   );
 }
---
+++ src/containers/SignIn/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from "./signIn.module.css"
+
+export default function SignIn() {
+    return (
+        <div>
+            <button className={classes.Button}>Click me</button>
+        </div>
+    )
+}
