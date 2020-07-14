86cd1fa4d6ed0c41266d9c01f3218f1c7f4bab41
INI\triet.le <triet.le@terralogic.com>
2020-07-14 08:47:44 +0700

    Add user input validation in 'AddTodoPopup' component

--- src/containers/ToDos/AddTodoPopup/index.js
+++ src/containers/ToDos/AddTodoPopup/index.js
@@ -4,7 +4,8 @@ import Popup from "../../../components/Popup"
 
 export default class AddTodoPopup extends Component {
     state={
-        userInput:""
+        userInput:"",
+        errMsg:""
     }
 
     changeUserInputHandler(event){
@@ -14,15 +15,32 @@ export default class AddTodoPopup extends Component {
     }
 
     submitInputHandler(){
-        this.props.addTodo(this.state.userInput)
-        this.setState({
-            userInput:""
-        })
+        if (this.validationHandler()) {
+            this.props.addTodo(this.state.userInput)
+            this.setState({
+                userInput:""
+            })
+        }
     }
-    render(props) {
+
+    validationHandler(){
+        if (this.state.userInput.length === 0) {
+            this.setState({
+                errMsg: "Invalid Input!"
+            })
+            return false
+        }
+        this.setState({
+            errMsg: ""
+        })
+        return true
+    }
+
+    render() {
         return (
             <Popup popupStatus={this.props.popupStatus}>
                 <div className={classes.AddTodoPopup}>
+                    <span className={classes.ErrorMsg}>{this.state.errMsg}</span>
                     <input className={classes.AddTodoField} value={this.state.userInput} type="text" onChange={(e)=>this.changeUserInputHandler(e)}/>
                     <button className={classes.Button} onClick={()=> this.submitInputHandler()}>Add</button>
                 </div>
